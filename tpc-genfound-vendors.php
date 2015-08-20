549e18222f89ed7ddc6181398e8a32686543c12b
Joey Hayes <bjoeyhayes@gmail.com>
2015-08-20 02:11:06 -0400

    plugin info

---
+++ tpc-genfound-vendors.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Great Dishes is a dead simple plugin to make a menu for a restaurant.
+ *
+ * @package   TPC Vendors
+ * @author    Joey Hayes
+ * @copyright 2015 Joey Hayes
+ * @license   GPL-2.0+
+ * @link      https://github.com/joeyred
+ *
+ * @wordpress-plugin
+ * Plugin Name:       TPC Vendors
+ * Plugin URI:        https://github.com/joeyred/tpc-genfound-vendors
+ * Description:       Simple Plugin for adding a custom post type for Vendors using theme stylings and built on Foundation for Sites 5.
+ * Version:           0.1.0
+ * Author:            Robin Cornett
+ * Author URI:        https://github.com/joeyred
+ * Text Domain:       tpc-genfound-vendors
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+*/
+?>
