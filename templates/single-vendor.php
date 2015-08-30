cb758a73b2628c4b8d5b827c73827321370c944b
Joey Hayes <bjoeyhayes@gmail.com>
2015-08-30 18:55:57 -0400

    added static markup

---
+++ templates/single-vendor.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+<head>
+  <meta charset="utf-8">
+  <!-- If you delete this meta tag World War Z will become a reality -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Foundation 5</title>
+
+  <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/foundation.css">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <!-- If you are using the gem version, you need this only -->
+  <link rel="stylesheet" href="css/app.css">
+
+  <script src="js/vendor/modernizr.js"></script>
+
+</head>
+<body>
+
+<!-- NAVIGATION -->
+<nav class="top-bar" data-topbar role="navigation">
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/index.html">Sandbox</a></h1>
+    </li>
+     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="active"><a href="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/vendor.html">Vendor</a></li>
+      <li class="has-dropdown">
+        <a href="#">Right Button Dropdown</a>
+        <ul class="dropdown">
+          <li><a href="#">First link in dropdown</a></li>
+          <li class="active"><a href="#">Active link in dropdown</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Left Nav Section -->
+    <ul class="left">
+      <li><a href="#">Left Nav Button</a></li>
+    </ul>
+  </section>
+</nav> <!-- END NAVIGATION -->
+
+<!-- LOGO HEADER -->
+
+  <img src="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/img/DIY_homepage.png" alt="Logo">
+
+
+<!-- ROW 1 - HEADING -->
+
+<div class="row"><!-- ROW OPEN -->
+
+  <div class="columns"><!-- Title - Right -->
+    <h2>Expert Video-based Tutorials that Insure You Provide Expert Service AND Get Paid!</h2>
+    </div>
+
+  <div class="columns"><!-- Media - Left -->
+
+    <div class="flex-video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/s4ItaOEyKiw" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+  </div>
+
+  <div class="columns"><!-- Subheader - Right -->
+
+  <h3>IRS Audits, Exams and Collections are Skyrocketing!</h3>
+
+  <h4>Make Sure Your Clients Get Expert Service in This Critical Area with!</h4>
+
+  <a href="#" class="button expand">Call to Action</a><!-- Call to Action Button 1 -->
+
+  </div>
+
+</div> <!-- ROW CLOSE -->
+
+<!-- ROW 2 - INFO BOXES -->
+
+<div class="row" data-equalizer> <!-- Use Equalizer Markup -->
+
+  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+    <div class="info-box">
+      <span class="fa fa-laptop"></span>  
+
+      <h3>Visual Tutorials</h3>
+
+      <p>Video and PDF eBook tutorials that give you exactly the information you need, as well as real world application and examples!</p>
+    </div> <!-- INFO BOX CLOSE -->
+  </div> <!-- COLUMN CLOSE -->
+
+  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+    <div class="info-box">
+      <span class="fa fa-laptop"></span>  
+
+      <h3>Visual Tutorials</h3>
+
+      <p>Video and PDF eBook tutorials that give you exactly the information you need, as well as real world application and examples!</p>
+    </div> <!-- INFO BOX CLOSE -->
+  </div> <!-- COLUMN CLOSE -->
+
+  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+    <div class="info-box">
+      <span class="fa fa-laptop"></span>  
+
+      <h3>Visual Tutorials</h3>
+
+      <p>Video and PDF eBook tutorials that give you exactly the information you need, as well as real world application and examples!</p>
+    </div> <!-- INFO BOX CLOSE -->
+  </div> <!-- COLUMN CLOSE -->
+
+  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+    <div class="info-box">
+      <span class="fa fa-laptop"></span>  
+
+      <h3>Visual Tutorials</h3>
+
+      <p>Video and PDF eBook tutorials that give you exactly the information you need, as well as real world application and examples!</p>
+    </div> <!-- INFO BOX CLOSE -->
+  </div> <!-- COLUMN CLOSE -->
+
+</div> <!-- ROW CLOSE -->
+
+<!-- ROW 3 - MEET THE STAFF -->
+
+<div class="row"><!-- ROW OPEN -->
+
+  <!-- INFO BOX 1 -->
+  <div class="columns medium-6">
+    <div class="row">
+      <div class="columns small-5">
+        <img src="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/img/allen2b.jpg" alt="Allen">
+      </div>
+      
+      <div class="columns small-7">
+        <h3>Allen Scioli</h3>
+        <h4>President</h4> 
+      </div>
+    </div>
+    <div class="columns small-12">
+      <p>A former field assistance tax specialist and revenue officer with the SB/SE division of the Internal Revenue Service. He has been involved in the federal taxation arena for over fourteen years, which includes practicing in a top national public accounting firm and the U.S. Government.</p>
+
+      <p>Since forming Scioli and Associates, a tax representation firm in Saginaw, Michigan, Mr. Scioli, a specialist in IRS Offers in Compromise, has saved clients millions of dollars with hundreds of approved offers. Additionally, Allen has served as an expert witness in many State Tax Tribunal cases, U.S. Bankruptcy & District Court cases, and U.S. Dept. of Justice cases dealing with the Tax Division. Mr. Scioli is a former U.S. Treasury Secretary Appointee and a member of the IRS's Taxpayer Advocacy Panel (TAP).</p>
+    </div>
+  </div>
+  <!-- INFO BOX 2 -->
+  <div class="columns medium-6">
+    <div class="row">
+      <div class="columns small-5">
+        <img src="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/img/allen2b.jpg" alt="Allen">
+      </div>
+      
+      <div class="columns small-7">
+        <h3>Allen Scioli</h3>
+        <h4>President</h4> 
+      </div>
+    </div>
+    <div class="columns small-12">
+      <p>A former field assistance tax specialist and revenue officer with the SB/SE division of the Internal Revenue Service. He has been involved in the federal taxation arena for over fourteen years, which includes practicing in a top national public accounting firm and the U.S. Government.</p>
+
+      <p>Since forming Scioli and Associates, a tax representation firm in Saginaw, Michigan, Mr. Scioli, a specialist in IRS Offers in Compromise, has saved clients millions of dollars with hundreds of approved offers. Additionally, Allen has served as an expert witness in many State Tax Tribunal cases, U.S. Bankruptcy & District Court cases, and U.S. Dept. of Justice cases dealing with the Tax Division. Mr. Scioli is a former U.S. Treasury Secretary Appointee and a member of the IRS's Taxpayer Advocacy Panel (TAP).</p>
+    </div>
+  </div>
+
+</div><!-- ROW CLOSE -->
+
+<!-- EXTRA CONTENT -->
+
+<div class="row"><!-- ROW OPEN -->
+
+</div><!-- ROW CLOSE -->
+
+<!-- MAIN CONTENT -->
+
+<div class="row"><!-- ROW OPEN -->
+
+</div><!-- ROW CLOSE -->
+
+
+  <!-- body content here -->
+
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
