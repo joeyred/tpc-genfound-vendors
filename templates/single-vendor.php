4cf0fcc17411f36494c9f0e02c5d81d24bb842db
Joey Hayes <bjoeyhayes@gmail.com>
2015-09-01 04:54:41 -0400

    added better responsiveness

--- templates/single-vendor.php
+++ templates/single-vendor.php
@@ -1,56 +1,4 @@
-<!DOCTYPE html>
-<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
-<html class="no-js" lang="en" >
-
-<head>
-  <meta charset="utf-8">
-  <!-- If you delete this meta tag World War Z will become a reality -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Foundation 5</title>
-
-  <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/foundation.css">
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
-  <!-- If you are using the gem version, you need this only -->
-  <link rel="stylesheet" href="css/app.css">
-
-  <script src="js/vendor/modernizr.js"></script>
-
-</head>
-<body>
-
-<!-- NAVIGATION -->
-<nav class="top-bar" data-topbar role="navigation">
-  <ul class="title-area">
-    <li class="name">
-      <h1><a href="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/index.html">Sandbox</a></h1>
-    </li>
-     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-  </ul>
-
-  <section class="top-bar-section">
-    <!-- Right Nav Section -->
-    <ul class="right">
-      <li class="active"><a href="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/vendor.html">Vendor</a></li>
-      <li class="has-dropdown">
-        <a href="#">Right Button Dropdown</a>
-        <ul class="dropdown">
-          <li><a href="#">First link in dropdown</a></li>
-          <li class="active"><a href="#">Active link in dropdown</a></li>
-        </ul>
-      </li>
-    </ul>
-
-    <!-- Left Nav Section -->
-    <ul class="left">
-      <li><a href="#">Left Nav Button</a></li>
-    </ul>
-  </section>
-</nav> <!-- END NAVIGATION -->
+<?php get_header(); ?>
 
 <!-- LOGO HEADER -->
 
@@ -61,11 +9,11 @@
 
 <div class="row"><!-- ROW OPEN -->
 
-  <div class="columns"><!-- Title - Right -->
+  <div class="columns small-12"><!-- Title - Right -->
     <h2>Expert Video-based Tutorials that Insure You Provide Expert Service AND Get Paid!</h2>
     </div>
 
-  <div class="columns"><!-- Media - Left -->
+  <div class="columns medium-6"><!-- Media - Left -->
 
     <div class="flex-video">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/s4ItaOEyKiw" frameborder="0" allowfullscreen></iframe>
@@ -73,7 +21,7 @@
 
   </div>
 
-  <div class="columns"><!-- Subheader - Right -->
+  <div class="columns medium-6"><!-- Subheader - Right -->
 
   <h3>IRS Audits, Exams and Collections are Skyrocketing!</h3>
 
@@ -89,7 +37,7 @@
 
 <div class="row" data-equalizer> <!-- Use Equalizer Markup -->
 
-  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+  <div class="columns small-12 medium-6 large-3" data-equalizer-watch> <!-- COLUMN -->
     <div class="info-box">
       <span class="fa fa-laptop"></span>  
 
@@ -99,7 +47,7 @@
     </div> <!-- INFO BOX CLOSE -->
   </div> <!-- COLUMN CLOSE -->
 
-  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+  <div class="columns small-12 medium-6 large-3" data-equalizer-watch> <!-- COLUMN -->
     <div class="info-box">
       <span class="fa fa-laptop"></span>  
 
@@ -109,7 +57,7 @@
     </div> <!-- INFO BOX CLOSE -->
   </div> <!-- COLUMN CLOSE -->
 
-  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+  <div class="columns small-12 medium-6 large-3" data-equalizer-watch> <!-- COLUMN -->
     <div class="info-box">
       <span class="fa fa-laptop"></span>  
 
@@ -119,7 +67,7 @@
     </div> <!-- INFO BOX CLOSE -->
   </div> <!-- COLUMN CLOSE -->
 
-  <div class="columns" data-equalizer-watch> <!-- COLUMN -->
+  <div class="columns small-12 medium-6 large-3" data-equalizer-watch> <!-- COLUMN -->
     <div class="info-box">
       <span class="fa fa-laptop"></span>  
 
@@ -135,8 +83,8 @@
 
 <div class="row"><!-- ROW OPEN -->
 
-  <!-- INFO BOX 1 -->
-  <div class="columns medium-6">
+  <!-- STAFF 1 -->
+  <div class="columns large-6">
     <div class="row">
       <div class="columns small-5">
         <img src="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/img/allen2b.jpg" alt="Allen">
@@ -153,8 +101,8 @@
       <p>Since forming Scioli and Associates, a tax representation firm in Saginaw, Michigan, Mr. Scioli, a specialist in IRS Offers in Compromise, has saved clients millions of dollars with hundreds of approved offers. Additionally, Allen has served as an expert witness in many State Tax Tribunal cases, U.S. Bankruptcy & District Court cases, and U.S. Dept. of Justice cases dealing with the Tax Division. Mr. Scioli is a former U.S. Treasury Secretary Appointee and a member of the IRS's Taxpayer Advocacy Panel (TAP).</p>
     </div>
   </div>
-  <!-- INFO BOX 2 -->
-  <div class="columns medium-6">
+  <!-- STAFF 2 -->
+  <div class="columns large-6">
     <div class="row">
       <div class="columns small-5">
         <img src="file:///Users/brianhayes/Sites/Foundation%20Projects/tests/img/allen2b.jpg" alt="Allen">
@@ -186,13 +134,39 @@
 
 </div><!-- ROW CLOSE -->
 
+<?php
+
+/*
+*  get all custom fields and dump for testing
+*/
+
+$fields = get_fields();
+var_dump( $fields ); 
+
+/*
+*  get all custom fields, loop through them and load the field object to create a label => value markup
+*/
+
+$fields = get_fields();
+
+if( $fields )
+{
+  foreach( $fields as $field_name => $value )
+  {
+    // get_field_object( $field_name, $post_id, $options )
+    // - $value has already been loaded for us, no point to load it again in the get_field_object function
+    $field = get_field_object($field_name, false, array('load_value' => false));
+
+    echo '<div>';
+      echo '<h3>' . $field_name . '</h3>';
+      echo $value;
+    echo '</div>';
+  }
+}
+
+?>
+
 
   <!-- body content here -->
 
-  <script src="js/vendor/jquery.js"></script>
-  <script src="js/foundation.min.js"></script>
-  <script>
-    $(document).foundation();
-  </script>
-</body>
-</html>
+<?php get_footer(); ?>  
