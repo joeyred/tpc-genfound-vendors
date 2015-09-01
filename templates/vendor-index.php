ac43b175be9c477c56c1e0891ac258521c848b11
Joey Hayes <bjoeyhayes@gmail.com>
2015-09-01 04:55:09 -0400

    static markup for search index shortcode

---
+++ templates/vendor-index.php
@@ -0,0 +1,102 @@
+<?php
+
+?>
+
+<div class="row">
+
+  <div class="large-12 columns">
+
+    <div class="radius panel">
+
+      <div class="row">
+
+        <div class="large-8 columns"> <!-- Search Form Column OPEN -->
+          <form> <!-- SEARCH FORM -->
+            <div class="row collapse"><!-- Search Form Inner Row OPEN -->
+
+              <div class="large-10 small-8 columns"> <!-- INPUT COLUMN -->
+                <input type="text"/>
+              </div>
+
+              <div class="large-2 small-3 columns"> <!-- BUTTON COLUMN -->
+                <a href="#" class="postfix button expand">Search</a>
+              </div>
+
+            </div> <!-- Search Form Inner Row CLOSE -->
+          </form>
+        </div> <!-- Search Form Column CLOSE --> 
+
+        <div class="large-2 columns"> <!-- Dropdown 1 -->
+
+          <button href="#" data-dropdown="drop1" aria-controls="drop1" aria-expanded="false" class="button small dropdown">Category</button><br>
+          <ul id="drop1" data-dropdown-content class="f-dropdown" aria-hidden="true">
+            <li><a href="#">Cat 1</a></li>
+            <li><a href="#">Cat 2</a></li>
+            <li><a href="#">Cat 3</a></li>
+          </ul>
+
+        </div>  
+
+        <div class="large-2 columns"> <!-- Dropdown 2 -->
+
+          <button href="#" data-dropdown="drop2" aria-controls="drop2" aria-expanded="false" class="button small dropdown">Service</button><br>
+          <ul id="drop2" data-dropdown-content class="f-dropdown" aria-hidden="true">
+            <li><a href="#">Service 1</a></li>
+            <li><a href="#">Service 2</a></li>
+            <li><a href="#">Service 3</a></li>
+          </ul>
+
+        </div>
+
+      </div>
+
+    </div> <!-- Panel CLOSE -->
+
+  </div>
+
+</div>
+
+
+
+
+
+
+<div class="row">
+
+  <div class="large-12 show-for-small columns">
+  <h3>Header</h3><hr>
+</div>
+
+  <div class="large-3 small-6 columns">
+    <img src="http://placehold.it/500x500&text=Thumbnail">
+    <div class="panel">
+      <p>Description</p>
+    </div>
+  </div>
+
+  <div class="large-3 small-6 columns">
+    <img src="http://placehold.it/500x500&text=Thumbnail">
+    <div class="panel">
+      <p>Description</p>
+    </div>
+  </div>
+
+  <div class="large-3 small-6 columns">
+    <img src="http://placehold.it/500x500&text=Thumbnail">
+    <div class="panel">
+      <p>Description</p>
+    </div>
+  </div>
+
+  <div class="large-3 small-6 columns">
+    <img src="http://placehold.it/500x500&text=Thumbnail">
+    <div class="panel">
+      <p>Description</p>
+    </div>
+  </div>
+
+</div>
+
+
+<?php
+
