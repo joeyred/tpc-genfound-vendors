7a6071be52b93bfdfe5951ad2746672d08388f67
Joey Hayes <bjoeyhayes@gmail.com>
2015-09-01 04:55:27 -0400

    added a shortcode

--- tpc-genfound-vendors.php
+++ tpc-genfound-vendors.php
@@ -118,18 +118,39 @@ function tpcvendors_create_taxonomies() {
 // Filter the single_template with our custom function 
 add_filter('single_template', 'tpcvendors_custom_single_template');
 
-//route single- template
+/**
+ * Display custom tempalte for single post
+ * 
+ * @param  string $single_template Original Path
+ * @return string                  Ammended Path
+ */
 function tpcvendors_custom_single_template( $single_template ){
+
   global $wp_query, $post;
+
   $found = locate_template('single-vendor.php');
-  if ( $post->post_type == 'vendor' && $found = '' ) {
-    //$single_template = plugins_url( 'tpc-genfound-vendors/templates/single-vendor.php' );
-    $single_template = dirname(__FILE__).'/templates/single-vendor.php';
+
+  if ( $post->post_type == 'vendor' ) {
+    $single_template = dirname(__FILE__) . '/templates/single-vendor.php';
   }
+
   return $single_template;
+
 }
 
+function tpcvendors_index_shortcode( $atts, $content = null ) {
 
+	global $post;
 
+	ob_start(); // Buffer
+
+	require( 'templates/vendor-index.php' );
+
+	$content = ob_get_clean(); // Set buffered function to object 
+
+	return $content; // Return object
+
+}
+add_shortcode( 'vendor_index', 'tpcvendors_index_shortcode' );
 
 ?>
